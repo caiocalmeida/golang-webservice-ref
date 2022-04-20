64d1497be908c3643696846ba52e53bf8c373a2e
unknown <40412377+caiocalmeida@users.noreply.github.com>
2022-04-20 00:26:54 -0300

    Add Swagger

--- internal/api/gin/router.go
+++ internal/api/gin/router.go
@@ -1,6 +1,11 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	docs "github.com/caiocalmeida/go-webservice-ref/docs"
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 type Router interface {
 	Start()
@@ -14,14 +19,22 @@ func NewRouter(uc UserController) Router {
 	return &router{uc: uc}
 }
 
+// @Title         Go Web API
+// @Version       0.1
+// @License.name  MIT
+// @License.url   https://opensource.org/licenses/MIT
 func (r *router) Start() {
 	e := gin.Default()
+
+	docs.SwaggerInfo.BasePath = ""
 
 	e.GET("/user", r.uc.getUsers)
 	e.GET("/user/:id", r.uc.getUser)
 	e.POST("/user", r.uc.postUser)
 	e.PUT("/user/:id", r.uc.putUser)
 	e.DELETE("/user/:id", r.uc.deleteUser)
+
+	e.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	e.Run()
 }
--- internal/api/gin/userController.go
+++ internal/api/gin/userController.go
@@ -25,10 +25,24 @@ func NewUserController(ur data.UserRepository) UserController {
 	return &userController{ur: ur}
 }
 
+// @Tags     User
+// @Produce  json
+// @Success  200  {array}   UserDto  "OK"
+// @Failure  404  {string}  string   "Not Found"
+// @Failure  500  {string}  string   "Internal Error"
+// @Router   /user [get]
 func (uc *userController) getUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, uc.ur.GetUsers())
 }
 
+// @Tags     User
+// @Produce  json
+// @Param    id   path      string   true  "User UUID"
+// @Success  200  {object}  UserDto  "OK"
+// @Failure  400  {string}  string   "Bad Request"
+// @Failure  404  {string}  string   "Not Found"
+// @Failure  500  {string}  string   "Internal Error"
+// @Router   /user/{id} [get]
 func (uc *userController) getUser(c *gin.Context) {
 	id, err := uuid.Parse(c.Param("id"))
 
@@ -45,6 +59,14 @@ func (uc *userController) getUser(c *gin.Context) {
 	c.Status(http.StatusNotFound)
 }
 
+// @Tags     User
+// @Accept   json
+// @Produce  json
+// @Param    userDTO  body      UserDto  true  "User data"
+// @Success  200      {object}  UserDto  "OK"
+// @Failure  400      {string}  string   "Bad Request"
+// @Failure  500      {string}  string   "Internal Error"
+// @Router   /user [post]
 func (uc *userController) postUser(c *gin.Context) {
 	userDto := &UserDto{}
 
@@ -60,6 +82,16 @@ func (uc *userController) postUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, addedUser)
 }
 
+// @Tags     User
+// @Accept   json
+// @Produce  json
+// @Param    id        path      string   true  "User UUID"
+// @Param    userData  body      UserDto  true  "User data"
+// @Success  200       {object}  UserDto  "OK"
+// @Failure  400       {string}  string   "Bad Request"
+// @Failure  404       {string}  string   "Not Found"
+// @Failure  500       {string}  string   "Internal Error"
+// @Router   /user/{id} [put]
 func (uc *userController) putUser(c *gin.Context) {
 	userDto := &UserDto{}
 	if err := c.ShouldBind(&userDto); err != nil {
@@ -76,6 +108,15 @@ func (uc *userController) putUser(c *gin.Context) {
 	c.JSON(http.StatusOK, uc.ur.UpdateUser(userDto.ToUser(id)))
 }
 
+// @Tags     User
+// @Accept   json
+// @Produce  json
+// @Param    id   path      string   true  "User UUID"
+// @Success  200  {object}  UserDto  "OK"
+// @Failure  400  {string}  string   "Bad Request"
+// @Failure  404  {string}  string   "Not Found"
+// @Failure  500  {string}  string   "Internal Error"
+// @Router   /user/{id} [delete]
 func (uc *userController) deleteUser(c *gin.Context) {
 	id, err := uuid.Parse(c.Param("id"))
 
